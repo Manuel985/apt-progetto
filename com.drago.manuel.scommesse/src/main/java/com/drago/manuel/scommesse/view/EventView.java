0c5acebd9524f4f208208f0b1b9584a1efdebd99
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-16 00:59:18 +0200

    Creazione interfaccia view

---
+++ com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/view/EventView.java
@@ -0,0 +1,19 @@
+package com.drago.manuel.scommesse.view;
+
+import java.util.List;
+
+import com.drago.manuel.scommesse.model.EventModel;
+
+public interface EventView {
+
+	void showAllEvents(List<EventModel> eventModels);
+
+	void showError(String message, EventModel eventModel);
+
+	void eventAdded(EventModel eventModel);
+
+	void eventRemoved(EventModel eventModel);
+
+	void eventChanged(EventModel eventModel);
+
+}
