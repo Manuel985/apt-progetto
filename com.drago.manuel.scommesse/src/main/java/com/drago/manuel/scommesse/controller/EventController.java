d5143f4ae0ce1117c2b2c349a254024aebf8f69a
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-16 11:29:44 +0200

    Creazione metodo per testare nuovo evento quando non è già presente

--- com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/controller/EventController.java
+++ com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.drago.manuel.scommesse.controller;
 
+import com.drago.manuel.scommesse.model.EventModel;
 import com.drago.manuel.scommesse.repository.EventRepository;
 import com.drago.manuel.scommesse.view.EventView;
 
@@ -15,6 +16,20 @@ public class EventController {
 
 	public void allEvents() {
 		eventView.showAllEvents(eventRepository.findAll());
+	}
+
+	public void newEvent(EventModel eventModel) {
+		EventModel existingEventModel = eventRepository.findByHomeAwayOutcome(eventModel.getHomeTeam(),
+				eventModel.getAwayTeam(), eventModel.getOutcome());
+		if (existingEventModel != null) {
+			eventView.showError("Already existing event " + eventModel.getHomeTeam() + "-" + eventModel.getAwayTeam()
+					+ " " + eventModel.getOutcome(), existingEventModel);
+			return;
+		}
+
+		eventRepository.save(eventModel);
+		eventView.eventAdded(eventModel);
+
 	}
 
 }
