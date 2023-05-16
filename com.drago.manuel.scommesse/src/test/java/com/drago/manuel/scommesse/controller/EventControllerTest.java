8db5121fa75bff6d3c15d45413885df57a2d2941
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-16 10:21:27 +0200

    Creazione test case per cotroller

---
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/controller/EventControllerTest.java
@@ -0,0 +1,19 @@
+package com.drago.manuel.scommesse.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
