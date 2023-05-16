d6b706337c9320c52b2fd00baa59509f242125c4
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-16 11:40:32 +0200

    Creazione metodo per testare nuovo evento quando è già presente

--- com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/controller/EventControllerTest.java
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/controller/EventControllerTest.java
@@ -1,8 +1,10 @@
 package com.drago.manuel.scommesse.controller;
 
 import static java.util.Arrays.asList;
+import static org.mockito.Mockito.ignoreStubs;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -43,13 +45,25 @@ public class EventControllerTest {
 		String homeTeam = "Juventus";
 		String awayTeam = "Inter";
 		String outcome = "X";
-		double odds = 3.20;
-		EventModel eventModel = new EventModel(homeTeam, awayTeam, outcome, odds);
+		EventModel eventModel = new EventModel(homeTeam, awayTeam, outcome, 3.20);
 		when(eventRepository.findByHomeAwayOutcome(homeTeam, awayTeam, outcome)).thenReturn(null);
 		eventController.newEvent(eventModel);
 		InOrder inOrder = inOrder(eventRepository, eventView);
 		inOrder.verify(eventRepository).save(eventModel);
 		inOrder.verify(eventView).eventAdded(eventModel);
+	}
+
+	@Test
+	public void testNewEventWhenEventAlreadyExist() {
+		String homeTeam = "Juventus";
+		String awayTeam = "Inter";
+		String outcome = "X";
+		EventModel newEventModel = new EventModel(homeTeam, awayTeam, outcome, 3.20);
+		EventModel existingEventModel = new EventModel(homeTeam, awayTeam, outcome, 2.20);
+		when(eventRepository.findByHomeAwayOutcome(homeTeam, awayTeam, outcome)).thenReturn(existingEventModel);
+		eventController.newEvent(newEventModel);
+		verify(eventView).showError("Already existing event Juventus-Inter X", existingEventModel);
+		verifyNoMoreInteractions(ignoreStubs(eventRepository));
 	}
 
 }
