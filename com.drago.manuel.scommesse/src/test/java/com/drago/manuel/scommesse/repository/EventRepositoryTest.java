a78a514fbe96d3de63e30e23a326cdd54cf9f156
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-17 16:23:09 +0200

    Creazione test case per repository

---
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/repository/EventRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.drago.manuel.scommesse.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
