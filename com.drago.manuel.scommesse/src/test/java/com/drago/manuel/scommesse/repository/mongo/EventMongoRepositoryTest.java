73ed5250eab789ab1a9fddf9bd68fe17bf9fe75b
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-17 16:45:53 +0200

    Creazione test case per repository mongo

---
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.drago.manuel.scommesse.repository.mongo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventMongoRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
