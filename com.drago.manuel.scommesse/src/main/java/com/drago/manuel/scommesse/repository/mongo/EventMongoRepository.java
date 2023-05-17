7aeb5b764a06932b1fc7b1cb0feff10ce156c32d
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-18 00:28:27 +0200

    Risoluzione code smells

--- com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepository.java
+++ com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepository.java
@@ -15,6 +15,10 @@ import com.mongodb.client.model.Updates;
 
 public class EventMongoRepository implements EventRepository {
 
+	private static final String ODDS = "odds";
+	private static final String OUTCOME = "outcome";
+	private static final String AWAY_TEAM = "awayTeam";
+	private static final String HOME_TEAM = "homeTeam";
 	private MongoCollection<Document> eventCollection;
 
 	public EventMongoRepository(MongoClient client, String databaseName, String collectionName) {
@@ -28,14 +32,14 @@ public class EventMongoRepository implements EventRepository {
 	}
 
 	private EventModel fromDocumentToEvent(Document d) {
-		return new EventModel(d.getString("homeTeam"), d.getString("awayTeam"), d.getString("outcome"),
-				d.getDouble("odds"));
+		return new EventModel(d.getString(HOME_TEAM), d.getString(AWAY_TEAM), d.getString(OUTCOME),
+				d.getDouble(ODDS));
 	}
 
 	@Override
 	public EventModel findByHomeAwayOutcome(String homeTeam, String awayTeam, String outcome) {
-		Document d = eventCollection.find(Filters.and(Filters.eq("homeTeam", homeTeam),
-				Filters.eq("awayTeam", awayTeam), Filters.eq("outcome", outcome))).first();
+		Document d = eventCollection.find(Filters.and(Filters.eq(HOME_TEAM, homeTeam),
+				Filters.eq(AWAY_TEAM, awayTeam), Filters.eq(OUTCOME, outcome))).first();
 		if (d != null)
 			return fromDocumentToEvent(d);
 		return null;
@@ -44,23 +48,23 @@ public class EventMongoRepository implements EventRepository {
 	@Override
 	public void save(EventModel eventModel) {
 		eventCollection.insertOne(
-				new Document().append("homeTeam", eventModel.getHomeTeam()).append("awayTeam", eventModel.getAwayTeam())
-						.append("outcome", eventModel.getOutcome()).append("odds", eventModel.getOdds()));
+				new Document().append(HOME_TEAM, eventModel.getHomeTeam()).append(AWAY_TEAM, eventModel.getAwayTeam())
+						.append(OUTCOME, eventModel.getOutcome()).append(ODDS, eventModel.getOdds()));
 
 	}
 
 	@Override
 	public void delete(String homeTeam, String awayTeam, String outcome) {
-		eventCollection.deleteOne(Filters.and(Filters.eq("homeTeam", homeTeam), Filters.eq("awayTeam", awayTeam),
-				Filters.eq("outcome", outcome)));
+		eventCollection.deleteOne(Filters.and(Filters.eq(HOME_TEAM, homeTeam), Filters.eq(AWAY_TEAM, awayTeam),
+				Filters.eq(OUTCOME, outcome)));
 
 	}
 
 	@Override
 	public void update(EventModel eventModel) {
-		eventCollection.updateOne(Filters.and(Filters.eq("homeTeam", eventModel.getHomeTeam()),
-				Filters.eq("awayTeam", eventModel.getAwayTeam()), Filters.eq("outcome", eventModel.getOutcome())),
-				Updates.set("odds", eventModel.getOdds()));
+		eventCollection.updateOne(Filters.and(Filters.eq(HOME_TEAM, eventModel.getHomeTeam()),
+				Filters.eq(AWAY_TEAM, eventModel.getAwayTeam()), Filters.eq(OUTCOME, eventModel.getOutcome())),
+				Updates.set(ODDS, eventModel.getOdds()));
 
 	}
 
