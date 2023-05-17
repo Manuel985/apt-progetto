870593a9509e1e0ac314cb8921ae6f5528783eac
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-17 17:00:45 +0200

    Creazione metodi gestione database

---
+++ com.drago.manuel.scommesse/src/main/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepository.java
@@ -0,0 +1,45 @@
+package com.drago.manuel.scommesse.repository.mongo;
+
+import java.util.List;
+
+import com.drago.manuel.scommesse.model.EventModel;
+import com.drago.manuel.scommesse.repository.EventRepository;
+import com.mongodb.MongoClient;
+
+public class EventMongoRepository implements EventRepository {
+
+	public EventMongoRepository(MongoClient client) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<EventModel> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EventModel findByHomeAwayOutcome(String homeTeam, String awayTeam, String outcome) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(EventModel eventModel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String homeTeam, String awayTeam, String outcome) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(EventModel eventModel) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepositoryTest.java
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/repository/mongo/EventMongoRepositoryTest.java
@@ -1,10 +1,58 @@
 package com.drago.manuel.scommesse.repository.mongo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.net.InetSocketAddress;
+
+import org.bson.Document;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+
 public class EventMongoRepositoryTest {
+
+	private static final String EVENT_COLLECTION_NAME = "event";
+	private static final String BET_DB_NAME = "bet";
+	private static MongoServer server;
+	private static InetSocketAddress serverAddress;
+	private MongoClient client;
+	private EventMongoRepository eventMongoRepository;
+	private MongoCollection<Document> eventCollection;
+
+	@BeforeClass
+	public static void setupServer() {
+		server = new MongoServer(new MemoryBackend());
+		serverAddress = server.bind();
+	}
+
+	@AfterClass
+	public static void shutdownServer() {
+		server.shutdown();
+	}
+
+	@Before
+	public void setup() {
+		client = new MongoClient(new ServerAddress(serverAddress));
+		eventMongoRepository = new EventMongoRepository(client);
+		MongoDatabase database = client.getDatabase(BET_DB_NAME);
+		database.drop();
+		eventCollection = database.getCollection(EVENT_COLLECTION_NAME);
+	}
+
+	@After
+	public void tearDown() {
+		client.close();
+	}
 
 	@Test
 	public void test() {
