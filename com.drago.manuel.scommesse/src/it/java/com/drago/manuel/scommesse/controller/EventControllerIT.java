29d1c5e9b44a6cfde217d4d397dbcb790b24068c
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-18 10:31:30 +0200

    Creazione metodo per inizializzare database

--- com.drago.manuel.scommesse/src/it/java/com/drago/manuel/scommesse/controller/EventControllerIT.java
+++ com.drago.manuel.scommesse/src/it/java/com/drago/manuel/scommesse/controller/EventControllerIT.java
@@ -1,14 +1,36 @@
 package com.drago.manuel.scommesse.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import org.junit.Test;
+import com.drago.manuel.scommesse.model.EventModel;
+import com.drago.manuel.scommesse.repository.EventRepository;
+import com.drago.manuel.scommesse.repository.mongo.EventMongoRepository;
+import com.drago.manuel.scommesse.view.EventView;
+import com.mongodb.MongoClient;
 
+@RunWith(MockitoJUnitRunner.class)
 public class EventControllerIT {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Mock
+	private EventView eventView;
+
+	private EventRepository eventRepository;
+
+	private EventController eventController;
+
+	private static final String BET_DB_NAME = "bet";
+	private static final String EVENT_COLLECTION_NAME = "event";
+
+	@Before
+	public void setUp() {
+		eventRepository = new EventMongoRepository(new MongoClient("localhost"), BET_DB_NAME, EVENT_COLLECTION_NAME);
+		for (EventModel eventModel : eventRepository.findAll()) {
+			eventRepository.delete(eventModel.getHomeTeam(), eventModel.getAwayTeam(), eventModel.getOutcome());
+		}
+		eventController = new EventController(eventRepository, eventView);
 	}
 
 }
