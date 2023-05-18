c243b7c29e59829808966c263c95ebab8bb30ffc
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-18 14:16:57 +0200

    Creazione test case per ui

---
+++ com.drago.manuel.scommesse/src/test/java/com/drago/manuel/scommesse/view/swing/EventSwingViewTest.java
@@ -0,0 +1,14 @@
+package com.drago.manuel.scommesse.view.swing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventSwingViewTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
