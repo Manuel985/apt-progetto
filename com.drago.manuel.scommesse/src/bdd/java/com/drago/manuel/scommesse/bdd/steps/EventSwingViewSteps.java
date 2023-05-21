5a678f43a8aa0a3a750261381d249bb041b11f54
Manuel Drago <drago.manuel2000@gmail.com>
2023-05-21 15:41:19 +0200

    Implementazione steps scenario aggiunta evento

--- com.drago.manuel.scommesse/src/bdd/java/com/drago/manuel/scommesse/bdd/steps/EventSwingViewSteps.java
+++ com.drago.manuel.scommesse/src/bdd/java/com/drago/manuel/scommesse/bdd/steps/EventSwingViewSteps.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 
 import org.assertj.swing.core.BasicRobot;
 import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
 import org.bson.Document;
@@ -21,6 +22,10 @@ import io.cucumber.java.en.When;
 
 public class EventSwingViewSteps {
 
+	private static final String ODDS = "2.15";
+	private static final String OUTCOME = "X";
+	private static final String AWAY_TEAM = "Inter";
+	private static final String HOME_TEAM = "Juventus";
 	private static final String DB_NAME = "bet";
 	private static final String COLLECTION_NAME = "event";
 	private MongoClient mongoClient;
@@ -39,16 +44,13 @@ public class EventSwingViewSteps {
 			window.cleanUp();
 	}
 
-	@Given("The database contains an event with home team {string} and away team {string} and outcome {string} and odds {string}")
-	public void the_database_contains_an_event_with_home_team_and_away_team_and_outcome_and_odds(String string,
-			String string2, String string3, String string4) {
-		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
-				.insertOne(new Document().append("homeTeam", string).append("awayTeam", string2)
-						.append("outcome", string3).append("odds", Double.parseDouble(string4)));
+	@Given("The database contains an event")
+	public void the_database_contains_an_event() {
+		addTestEventToDatabase(HOME_TEAM, AWAY_TEAM, OUTCOME, ODDS);
 	}
 
-	@When("The Student View is shown")
-	public void the_Student_View_is_shown() {
+	@Given("The Event View is shown")
+	public void the_Event_View_is_shown() {
 		application("com.drago.manuel.scommesse.app.swing.EventSwingApp")
 				.withArgs("--db-name=" + DB_NAME, "--db-collection=" + COLLECTION_NAME).start();
 		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
@@ -59,10 +61,28 @@ public class EventSwingViewSteps {
 		}).using(BasicRobot.robotWithCurrentAwtHierarchy());
 	}
 
-	@Then("The list contains an element with home team {string} and away team {string} and outcome {string} and odds {string}")
-	public void the_list_contains_an_element_with_home_team_and_away_team_and_outcome_and_odds(String string,
-			String string2, String string3, String string4) {
-		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains(string, string2, string3, string4));
+	@Given("The user provides event data in the text fields")
+	public void the_user_provides_event_data_in_the_text_fields() {
+		window.textBox("homeTeamTextBox").enterText("Roma");
+		window.textBox("awayTeamTextBox").enterText("Lazio");
+		window.textBox("outcomeTextBox").enterText("1");
+		window.textBox("oddsTextBox").enterText("1.65");
+	}
+
+	@When("The user clicks the {string} button")
+	public void the_user_clicks_the_button(String string) {
+		window.button(JButtonMatcher.withText(string)).click();
+	}
+
+	@Then("The list contains the new event")
+	public void the_list_contains_the_new_event() {
+		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains("Roma", "Lazio", "1", "1.65"));
+	}
+
+	private void addTestEventToDatabase(String homeTeam, String awayTeam, String outcome, String odds) {
+		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
+				.insertOne(new Document().append("homeTeam", homeTeam).append("awayTeam", awayTeam)
+						.append("outcome", outcome).append("odds", Double.parseDouble(odds)));
 	}
 
 }
